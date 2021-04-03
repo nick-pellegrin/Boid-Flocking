2237a995788eddc79fd728cc35aff3158dc41764
Nick Pellegrin <nicholaspellegrin@yahoo.com>
2021-04-03 16:53:31 -0700

    simplifying move function

--- simulation.py
+++ simulation.py
@@ -45,6 +45,9 @@ Contents of python file:
 
 import pygame
 import sys
+import math
+from random import *
+import numpy as np
 
 
 
@@ -68,20 +71,29 @@ Contains the instructions for the movement mechanics of a single particle.
 
 ------------------------------------------------------------------------------------------
 """
+      
 
 class Particle:
     
-    PARTICLE_SIZE = 5  #in pixels
+    PARTICLE_SIZE = 5     #in pixels
+    NEIGHBOR_RADIUS = 10  #in pixels
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, is_leader):
         """
         initializes particle object |
-        x = starting x coord |
-        y = starting y coord |
+        x = (int) starting x coord |
+        y = (int) starting y coord |
+        is_leader = (boolean) is particle a leader |
         """
         self.x = x
         self.y = y
-        self.IMG = pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.PARTICLE_SIZE, 0)
+        self.is_leader = is_leader
+        self.velocity = [0, 0]
+        self.acceleration = [0, 0]
+
+        if self.is_leader: self.color = (255, 0, 0)
+        else: self.color = (255, 255, 255)
+        self.IMG = pygame.draw.circle(screen, self.color, (self.x, self.y), self.PARTICLE_SIZE, 0)
 
     
     def get_pos(self):
@@ -96,7 +108,7 @@ class Particle:
         draws particle on screen at (x, y) |
         returns: none |
         """
-        pygame.draw.circle(screen, (255, 255, 255), (x, y), self.PARTICLE_SIZE, 0)
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), self.PARTICLE_SIZE, 0)
     
    
     def move(self, pos_at, pos_to):
@@ -106,56 +118,30 @@ class Particle:
         pos_to = desired location (x_to, y_to) |
         returns: none |
         """
-        x_at, y_at = pos_at
-        x_to, y_to = pos_to
+        if self.is_leader:
+            x_at, y_at = pos_at
+            x_to, y_to = pos_to
 
-        while 1:
+            dist_vector = [x_to - x_at, y_to - y_at]  # vector starting at 'pos_at' going to to 'pos_to'
+            norm = np.linalg.norm(dist_vector)        # finds the norm (magnitude) of dist_vector
+            self.velocity =  3*(dist_vector/norm)          # a unit vecotr pointing to destination
+
+            self.x += self.velocity[0]
+            self.y += self.velocity[1]
             
-            if x_to == x_at or y_to == y_at: break   #stops loop when particle reaches destination      
-            
-            slope = (y_to - y_at) / (x_to - x_at) #sets slope (needs to be inverted for vertical movements)
-                
-            # for more horizontal movement 
-            if abs(slope) <= 1:           
-                if x_to > x_at:            
-                    x_inc = 1              
-                    y_inc = slope          
-                if x_to < x_at:            
-                    x_inc = -1             
-                    y_inc = -slope   
-            
-            # for more vertical movement
-            elif abs(slope) > 1:          
-                
-                # if slope is positive
-                if slope > 0:                  
-                    if x_to > x_at:            
-                        x_inc = 1/slope        
-                        y_inc = 1              
-                    if x_to < x_at:            
-                        x_inc = -1/slope       
-                        y_inc = -1     
-
-                # if slope is negative
-                if slope < 0:              
-                    if x_to > x_at:            
-                        x_inc = -1/slope        
-                        y_inc = -1              
-                    if x_to < x_at:            
-                        x_inc = 1/slope       
-                        y_inc = 1               
-                    
-
-            self.x = x_at + x_inc
-            self.y = y_at + y_inc
-            x_at = x_at + x_inc
-            y_at = y_at + y_inc
-
-            screen.fill(BACKGROUND_COLOR)
             self.draw(self.x, self.y)
-            pygame.display.update()
-            pygame.time.delay(5)
-            
+
+
+    def get_direction(self):
+        """
+        gets direction of the particle |
+        returns: [x, y] |
+        """
+
+        return self.velocity
+        
+        
+        
             
         
 
@@ -173,23 +159,40 @@ using pygame to create the simulation, and python object to represent the partic
 ------------------------------------------------------------------------------------------
 """
 
-#declares and initializes our first particle
-p_1 = Particle(550, 350)
+
+#declares and initializes our particles
+particle_list = []
+for i in range(100):
+    particle_list.append(Particle(randint(50, 1050), randint(50, 650), True))
+
 
 
 #main simulation loop
 while 1: 
-    for event in pygame.event.get():
+    # for event in pygame.event.get():
         
-        if event.type == pygame.QUIT:
-            sys.exit()
+    #     if event.type == pygame.QUIT:
+    #         sys.exit()
 
         
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            p_1.move( p_1.get_pos(), pygame.mouse.get_pos() )
+    #     if event.type == pygame.MOUSEBUTTONDOWN:
+    #         p_1.move( p_1.get_pos(), pygame.mouse.get_pos() )
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+
+    for i in particle_list:
+        i.move( i.get_pos(), pygame.mouse.get_pos() )
+        
     
     
     pygame.display.update()
+    screen.fill(BACKGROUND_COLOR)
+    pygame.time.delay(15)
+    
 
 
 
